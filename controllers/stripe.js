7c9855dacd0c46e8cc168afe3b82e9a5d1690387
jimdev90 <jimpv100190@gmail.com>
2023-09-13 20:41:39 -0500

    server

--- controllers/stripe.js
+++ controllers/stripe.js
@@ -1,5 +1,6 @@
 import User from "../models/user";
 import Stripe from 'stripe';
+import queryString from "query-string";
 
 const stripe = Stripe(process.env.STRIPE_SECRET);
 
@@ -9,29 +10,24 @@ export const createConnectAccount = async (req, res) => {
     let user = await User.findById(req.auth._id).exec();
     if (!user.stripe_account_id) {
         const account = await stripe.accounts.create({
-            type: 'express',
+            type: 'standard',
         });
         console.log("ACCOUNT =====>", account);
         user.stripe_account_id = account.id;
         user.save();
     }
+    console.log('USER', user);
+    let accountLink = await stripe.accountLinks.create({
+        account: user.stripe_account_id,
+        refresh_url: process.env.STRIPE_REDIRECT_URL,
+        return_url:  process.env.STRIPE_REDIRECT_URL,
+        type: 'account_onboarding'
+    });
 
-    console.log(user);
+    // accountLink = Object.assign({
+    //     'stripe_user[email]': user.email || undefined,
+    // });
 
-
-
-   
-
-   
-
-    // try {
-
-
-
-
-    // } catch (err) {
-    //     console.log('LOGIN ERROR ', err)
-    //     res.status(400).send('Inicio de sesión fallida');
-    // }
+    // console.log('ACCOUNT_LINK', accountLink);
 
 }
